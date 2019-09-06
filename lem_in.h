2849a19bbbad79cebdcc524d972195c9724e0907
villabryan88 <villabryan88@gmail.com>
2019-09-06 15:19:59 -0700

    moved error checking to happen during parsing to get rid of repeated logic

--- lem_in.h
+++ lem_in.h
@@ -6,7 +6,7 @@
 /*   By: bvilla <bvilla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 17:09:29 by bvilla            #+#    #+#             */
-/*   Updated: 2019/09/06 10:41:57 by bvilla           ###   ########.fr       */
+/*   Updated: 2019/09/06 12:52:05 by bvilla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,18 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-typedef struct	s_graph {
-	t_map		*adj_map;
-	char		*s;
-	char		*t;
-}				t_graph;
+typedef struct		s_graph {
+	t_map			*adj_map;
+	char			*s;
+	char			*t;
+}					t_graph;
 
-typedef struct	s_edge {
-	char		*loc;
-	char		taken;
-}				t_edge;
+typedef struct		s_edge {
+	char			*loc;
+	char			taken;
+	struct s_edge 	*next;
+}					t_edge;
 
-t_map			*map_init(void);
-void			kill(char print);
-void			**map_find(t_map *map, char *key);
-void			map_insert(t_map *adj_map, char *key, void *val);
-void			add_graph_node(t_graph *graph, char *key);
+void				kill(char print);
+void				add_graph_node(t_graph *graph, char *key);
 #endif
--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 /*   By: bvilla <bvilla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 21:12:38 by bvilla            #+#    #+#             */
-/*   Updated: 2019/09/06 09:10:55 by bvilla           ###   ########.fr       */
+/*   Updated: 2019/09/06 15:12:15 by bvilla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lem_in.h>
 #define	SET_ME "#set_me"
+#define MAP_SIZE 40000
 
 void	kill(char print){
 	ft_printf("ERROR\n");
@@ -20,143 +21,84 @@ void	kill(char print){
 	exit(0);
 }
 
-char	*get_start_end(char *line){
-	char	*st;
-	char	**tmp;
-
-	tmp = ft_strsplit(line, ' ');
-	if (!tmp || !(st = ft_strdup(tmp[0])))
-	{
-		kill(1);
-		return (NULL);
-	}
-	ft_strarrdel(tmp);
-	return (st);
-}
-void	handle_commands(char **s, char **t, char *line){
-	if (ft_strequ("##start", line))
-		*s = SET_ME;
-	else if (ft_strequ("##end", line))
-		*t = SET_ME;
-	else if (ft_strequ(*s, SET_ME))
-		*s = get_start_end(line);
-	else if (ft_strequ(*t, SET_ME))
-		*t = get_start_end(line);
-}
-
-int		hashtag_error_check(char *line, int rooms_done_flag){
-	static char	start_flag = 0;
-	static char	end_flag = 0;
-
-	if (ft_strnequ(line, "##", 2)){
-		if (rooms_done_flag)
-			return (1);
-		if (!start_flag && ft_strequ("##start", line))
-			start_flag = 1;
-		else if (!end_flag && ft_strequ("##end", line))
-			end_flag = 1;
-		else
-			return (1);
-	}
-	return (0);
-}
-
-int		room_error_check(char *line, 
-				char *rooms_done_flag, t_graph	*const	graph)
-{
-	char	**tmp;
-	int		err = 0;
-
-	tmp = ft_strsplit(line, ' ');
-	if (ft_arrlen(tmp, sizeof(void*)) == 3)
-	{
-		if (!ft_strisint(tmp[1]) || !ft_strisint(tmp[2]) || tmp[0][0] == 'L')
-			err = 1;
-		if (map_find(graph->adj_map, tmp[0]))
-			err = 1;
-	}
-	else if (graph->s && graph->t && 
-				!ft_strequ(SET_ME, graph->s) && !ft_strequ(SET_ME, graph->t))
-		*rooms_done_flag = 1;
-	else
-		err = 1;
-	ft_strarrdel(tmp);
-	return (err);
-}
-
-int		links_error_check (char *line){
-	char	**tmp;
-	int		err = 0;
-
-	tmp = ft_strsplit(line, '-');
-	if (ft_arrlen(tmp, sizeof(void*)) == 2)
-		(void)line;
-	else
-		err = 1;
-	ft_strarrdel(tmp);
-	return (err);	
-}
-
-int		err_and_zone_check(char *line, t_graph	*const	graph, int n,
-									char *rooms_done_flag)
-{
-	if (n == -1){
-		return (!ft_strisint(line) || ft_atoi(line) < 0);
-	}
-	if (ft_strnequ("#", line, 1)){
-		if (ft_strequ(graph->s, SET_ME) || ft_strequ(graph->t, SET_ME))
-			return (1);
-		return hashtag_error_check(line, *rooms_done_flag);
-	}
-	if (!*rooms_done_flag)
-	{
-		if (room_error_check(line, rooms_done_flag, graph))
-			return (1);
-	}
-	if (*rooms_done_flag)
-		return links_error_check(line);
-	return (0);
-}
-
 t_graph	*graph_init(){
 	t_graph	*new = malloc(sizeof(t_graph));
 
-	new->adj_map = map_init();
+	new->adj_map = map_init(MAP_SIZE);
 	new->s = NULL;
 	new->t = NULL;
 	return (new);
 }
 
-void	handle_links(char *line){
-	(void)line;
+void	handle_edges(char *line){
+	char	**tmp;
+
+	tmp = ft_strsplit(line, '-');
+	if (ft_arrlen(tmp, sizeof(void*)) == 2)
+		(void)line; // add edge to dictionary
+	else
+		kill(0);
+	ft_strarrdel(tmp);
 }
 
-void	handle_rooms(char *line, t_graph *const graph)
+void	handle_ants(char *line, int *n)
+{
+	if (!ft_strisint(line))
+		kill(1);
+	if (ft_atoi(line) < 0)
+		kill(0);
+	*n = ft_atoi(line);
+}
+
+void	handle_hashtag(char * line, t_graph *graph, char rooms_done_flag)
+{
+	char	**to_set;
+
+	to_set = (char**)"abort";
+	if (!ft_strnequ("##", line, 2))
+	{
+		if (ft_strequ(graph->s, SET_ME) || ft_strequ(graph->t, SET_ME))
+			kill(0);
+	}
+	else {
+		if (rooms_done_flag)
+			kill(0);
+		if (ft_strequ("##start", line))
+			to_set = &graph->s;
+		else if (ft_strequ("##end", line))
+			to_set = &graph->t;
+		if (*to_set)
+			kill(0);
+		*to_set = SET_ME;
+	}
+}
+
+void	handle_rooms(char *line, t_graph *const graph, char *rooms_done_flag)
 {
 	char	**tmp;
 
-	tmp = ft_strsplit(line, ' ');
-	(void)graph;
-	add_graph_node(graph, tmp[0]);
-}
-
-void	handle_input(char *line, t_graph *const graph, int *n,
-										char *rooms_done_flag)
-{
-	if (ft_strnequ("##", line, 2) || ft_strequ(SET_ME, graph->s) ||
-										ft_strequ(SET_ME, graph->t))
-		handle_commands(&graph->s, &graph->t, line);
-	if (*n == -1)
-		*n = ft_atoi(line);
-	else if (!ft_strnequ("#", line, 1))
+	if (!(tmp = ft_strsplit(line, ' ')))
+		kill(1);
+	if (ft_arrlen(tmp, sizeof(void*)) == 3)
 	{
-		if (rooms_done_flag)
-			handle_links(line);
-		else
-			handle_rooms(line, graph);
+		if (!ft_strisint(tmp[1]) || !ft_strisint(tmp[2]) || tmp[0][0] == 'L')
+			kill(0);
+		if (map_find(graph->adj_map, tmp[0], ft_strlen(tmp[0])))
+			kill(0);
+		if (ft_strequ(SET_ME, graph->s))
+			if (!(graph->s = ft_strdup(tmp[0])))
+				kill(1);
+		if (ft_strequ(SET_ME, graph->t))
+			if (!(graph->t = ft_strdup(tmp[0])))
+				kill(1);
+		//add room to dictionary
 	}
-	if (!ft_strnequ("#", line, 1) || ft_strnequ("##", line, 2))
-		ft_printf("%s\n", line);
+	else if (graph->s && graph->t && 
+				!ft_strequ(SET_ME, graph->s) && !ft_strequ(SET_ME, graph->t))
+		*rooms_done_flag = 1;
+	else
+		kill(0);
+	ft_strarrdel(tmp);
 }
 
 int		main(void)
@@ -164,15 +106,27 @@ int		main(void)
 	char			*line;
 	t_graph	*const	graph = graph_init();
 	int				n = -1;
-	char			rooms_done_flag = 0;
+	char			rooms_done_flag;
 
+	rooms_done_flag = 0;
 	while (get_next_line(0, &line) > 0)
 	{
-		if (!line || err_and_zone_check(line, graph, n, &rooms_done_flag))
-			kill(0);
-		handle_input(line, graph, &n, &rooms_done_flag);
-		free(line);
+		if (n == -1)
+			handle_ants(line, &n);
+		else if (ft_strnequ("#", line, 1))
+			handle_hashtag(line, graph, rooms_done_flag);
+		else
+		{
+			if (!rooms_done_flag)
+				handle_rooms(line, graph, &rooms_done_flag);
+			if (rooms_done_flag)
+				handle_edges(line);
+		}
+		if (!ft_strnequ("#", line, 1) || ft_strnequ("##", line, 2))
+			ft_printf("%s\n", line);
 	}
+	if (!line)
+		kill(1);
 	ft_printf("s: %s, t: %s\n", graph->s, graph->t);
 	return (0);
 }
